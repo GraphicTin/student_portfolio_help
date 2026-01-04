781a777e83948bc2a8e72d23cc0d2361c4d3ca05
tinn <fgbt4004@gmail.com>
2026-01-04 19:46:51 +0700

    eh

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
         }
     },
     base: '/student_portfolio_help/',
-    build: {
-        minify: 'terser', // Use 'terser' for maximum compression (slower build, smaller output)
-        sourcemap: false, // Turn off sourcemaps in production builds (unless debugging)
-        cssCodeSplit: true, // Should be true by default, ensures CSS is loaded with its component chunk
-    },
+    // build: {
+    //     minify: 'terser', // Use 'terser' for maximum compression (slower build, smaller output)
+    //     sourcemap: false, // Turn off sourcemaps in production builds (unless debugging)
+    //     cssCodeSplit: true, // Should be true by default, ensures CSS is loaded with its component chunk
+    // },
 })
